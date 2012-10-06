d40cb8a4ebac7948eae90c112c89080a5b7ca081
Daniel Portales <dportalesr@gmail.com>
2012-10-06 03:35:50 -0500

    Album restored maxsize 2

--- app/models/albumimg.php
+++ app/models/albumimg.php
@@ -1,7 +1,7 @@
 <?php
 class Albumimg extends AppModel {
 	var $name = 'Albumimg';
-	var $actsAs = array('File'=>array('portada'=>'album_id','fields'=>array('src'=>array('maxsize'=>2000))));
+	var $actsAs = array('File'=>array('portada'=>'album_id','fields'=>array('src'=>array('maxsize'=>358400))));
 	var $belongsTo = array(
 		'Album' => array(
 			'className'=>'Album',
